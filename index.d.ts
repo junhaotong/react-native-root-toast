6dc6dcccde87aec9c5a028aa72883fee437408e3
白酒 <15869294164@163.com>
2018-07-25 19:27:16 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,40 @@
-import { positions, durations } from './lib/ToastContainer';
-import { Manager } from './lib/Toast';
+import * as React from 'react';
+import {TextStyle,StyleProp,ViewStyle} from 'react-native';
+import RootSiblings from 'react-native-root-siblings';
 
-export default class RootToast {
-    static positions: positions;
-    static durations: durations;
-    static displayName: string;
+declare module "react-native-root-toast" {
+    interface ToastOptions {
+        duration?: number
+        visible?: boolean,
+        position?: number,
+        animation?: boolean,
+        shadow?: boolean,
+        backgroundColor?: string,
+        shadowColor?: string,
+        textColor?: string,
+        delay?: number,
+        hideOnPress?: boolean,
+        onHide?: Function,
+        onHidden?: Function,
+        onShow?: Function,
+        onShown?: Function
+    }
 
-    static show(message: string, options: object): void;
-    static hide(target: Manager): void;
+    export interface Durations {
+        SHORT: number
+        LONG: number,
+    }
+
+    export interface Positions {
+        TOP: number,
+        CENTER: number,
+        BOTTOM: number,
+    }
+
+    export default class Toast extends React.Component<ToastOptions> {
+        static show: (message: string, options?: ToastOptions) => RootSiblings;
+        static hide: (toast: RootSiblings) => void;
+        static durations: Durations;
+        static positions: Positions;
+    }
 }
